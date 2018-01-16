175f3fb3576f9a48b0a59355114c29e6de9865d2
krishnapsarthi <krishnas@alohatechnology.com>
2018-01-16 16:58:12 +0530

    Ran prettier for formatting

--- server/config/appConfig.js
+++ server/config/appConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-    TOKEN_SECRET: process.env.TOKEN_SECRET,
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET
-}
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+};
--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,10 +1,10 @@
 ﻿var mysql = require('mysql');
 
 var pool = mysql.createPool({
-    connectionLimit: 10,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.HOST
+  connectionLimit: 10,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.HOST,
 });
 
 module.exports = pool;
